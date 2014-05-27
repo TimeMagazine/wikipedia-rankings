4d9354af3cf1682f48f0fbed6beeff6f9833ace0
Chris Wilson <christopher.e.wilson@gmail.com>
2014-05-27 10:03:29 -0400

    +data

---
+++ wikipedia.r
@@ -0,0 +1,17 @@
+library("car")
+library("MASS")
+library("RJSONIO")
+
+data <- read.csv("people.csv")
+
+write(toJSON(sapply(data[-(1:5)],mean)), file="mean.json")
+write(toJSON(sapply(data[-(1:5)],sd)), file="sd.json")
+
+standardized = as.data.frame(scale(data[-(1:5)]))
+
+
+ranking = prcomp(standardized)
+
+
+write(toJSON(ranking$rotation), file="pca.json")
+write.csv(ranking$rotation, file="pca.csv")
